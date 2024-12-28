6bc72eac07f88a1c1ed327513a7453e24bef5233
Chaitanya Inamdar <susmiti63@gmail.com>
2024-12-27 21:11:32 -0500

    Merge Strings Alternately updated

--- Arrays & Strings/Merge Strings Alternately.py
+++ Arrays & Strings/Merge Strings Alternately.py
@@ -60,4 +60,39 @@ class Solution:
 
 
 # Time - O(N)
-# Space - O(1)
+# Space - O(A+B)
+
+Solutiom 2 - 
+
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        
+        A,B = len(word1), len(word2)
+        a,b = 0,0 # As there always gonna be 1 character in the string
+        result = []
+        word = 1
+        while a < A and b < B:
+
+            if word == 1:
+                result.append(word1[a])
+                a += 1
+                word = 2
+            else:
+                result.append(word2[b])
+                b += 1
+                word = 1
+        
+        while a < A:
+            result.append(word1[a])
+            a += 1
+
+        while b < B:
+            result.append(word2[b])
+            b += 1
+        
+        return "".join(result)
+
+# Time = O(A+B)
+# Space = O(A+B)
+
