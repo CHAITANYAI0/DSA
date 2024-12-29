beca2f8507530784956f28b1059675d81f74b7c4
Chaitanya Inamdar <susmiti63@gmail.com>
2024-12-29 15:21:08 -0500

    Summary Ranges

---
+++ Summary Ranges.py
@@ -0,0 +1,81 @@
+228. Summary Ranges
+Solved
+Easy
+Topics
+Companies
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b
+ 
+
+Example 1:
+
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+Example 2:
+
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9"
+ 
+
+Constraints:
+
+0 <= nums.length <= 20
+-231 <= nums[i] <= 231 - 1
+All the values of nums are unique.
+nums is sorted in ascending order.
+
+
+
+# Solution - 
+
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        #res.append(str(nums[i])+ "->"+str(nums[i+1]))
+
+        start = 0
+        res = []
+
+        for i in range(len(nums)):
+
+            if i == len(nums)-1 or nums[i+1] - nums[i] != 1:
+
+                if nums[start] == nums[i]:
+                    res.append(str(nums[start]))
+                else:  
+                    res.append(str(nums[start])+"->"+str(nums[i]))
+                start = i+1
+            
+        return res
+    
+# Time - O(N) - number of elemenets in the array
+# Space - O(N) - number of elements in the result array - worst case all the elements are isolated
+
+
+            
+                
+                
+
+            
+
+           
+        
+
+
+            
